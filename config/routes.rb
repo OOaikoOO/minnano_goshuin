9057b9555e056ed00595414604f54a3803953947
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-05-10 07:56:03 +0000

    [Create]user/adminモデル、コントローラ、View、routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   get 'top' => 'homes#top'
   get 'about' => 'homes#about'
+  
+  # ユーザー用
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
