c365842b4a30fcc11870a5fb42b6809f0b6c7d24
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-05-10 16:45:41 +0000

    [Create]新規投稿機能

---
+++ app/controllers/public/posts_controller.rb
@@ -0,0 +1,32 @@
+class Public::PostsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :address)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,21 @@ Rails.application.routes.draw do
   # ユーザー用
   root to: 'public/homes#top'
   get '/about' => 'public/homes#about'
-  
+
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
   
+  # resourcesを使用する際にURLにnamespaceを含めずにルーティングを設定する
+  scope module: :public do
+    resources :posts
+  end
+
   # 管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
