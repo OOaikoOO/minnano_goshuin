89d00f8a33fec877a03d27d9d5e0f5ea9e716531
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-05-13 17:01:24 +0000

    [Create]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Rick = User.find_or_create_by!(email: "rick@example.com") do |user|
+  user.name = "Rick"
+  user.password = "password"
+end
+
+Carl = User.find_or_create_by!(email: "Carl@example.com") do |user|
+  user.name = "Carl"
+  user.password = "password"
+end
+
+Daryl = User.find_or_create_by!(email: "Daryl@example.com") do |user|
+  user.name = "Daryl"
+  user.password = "password"
+end
+
+Carol = User.find_or_create_by!(email: "Carol@example.com") do |user|
+  user.name = "Carol"
+  user.password = "password"
+end
+
+Maggie = User.find_or_create_by!(email: "maggie@example.com") do |user|
+  user.name = "Maggie"
+  user.password = "password"
+end
+
+Michonne = User.find_or_create_by!(email: "michonne@example.com") do |user|
+  user.name = "Michonne"
+  user.password = "password"
+end
