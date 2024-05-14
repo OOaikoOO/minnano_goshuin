54a75a4418d48ca8a1fa30731687a8f994b31719
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-05-14 10:04:33 +0000

    [Create]searchコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'searches/search'
+  end
+  namespace :admin do
+    get 'searches/search'
+  end
   # ユーザー用
   root to: 'public/homes#top'
   get '/about' => 'public/homes#about'
@@ -7,21 +13,27 @@ Rails.application.routes.draw do
   get '/users/check' => 'public/users#check', as: :check_user
   patch '/users/withdraw' => 'public/users#withdraw', as: :withdraw_user
 
+  # ユーザー用ログイン関連
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-
+  
   # resourcesを使用する際にURLにnamespaceを含めずにルーティングを設定する
   scope module: :public do
+    get "search" => "searches#search"
     resources :posts
     resources :users
   end
 
-  # 管理者用
+  # 管理者用ログイン関連
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    get "search" => "searches#search"
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
