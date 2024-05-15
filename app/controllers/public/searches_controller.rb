4b413dede764d9f04aa07da7bcd10965d45823ea
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-05-15 15:16:08 +0000

    [Add]所在地検索機能

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -6,9 +6,12 @@ class Public::SearchesController < ApplicationController
     @content=params[:content]
     @method=params[:method]
     if @model == 'user'
-      @records=User.search_for(@content,@method)
+      @records=User.search_for(@content, @method)
+    elsif @model == 'address'
+      # postモデルで使用しているfieldパラメータのデフォルトは'title'のため明示的に'address'を指定する
+      @records=Post.search_for(@content, @method, 'address')
     else
-      @records=Post.search_for(@content,@method)
+      @records=Post.search_for(@content, @method)
     end
   end
 end
