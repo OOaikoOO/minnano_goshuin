cc243fb4197cd455ae4eb662996e3911906c90a0
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-05-20 19:00:09 +0000

    [Add]コメント検索機能

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,16 +2,16 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-    @model=params[:model]
-    @content=params[:content]
-    @method=params[:method]
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
     if @model == 'user'
-      @records=User.search_for(@content, @method)
+      @records = User.search_for(@content, @method)
     elsif @model == 'address'
       # postモデルで使用しているfieldパラメータのデフォルトは'title'のため明示的に'address'を指定する
-      @records=Post.search_for(@content, @method, 'address')
+      @records = Post.search_for(@content, @method, 'address')
     else
-      @records=Post.search_for(@content, @method)
+      @records = Post.search_for(@content, @method)
     end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+    def self.search_for(content, method)
+    if method == 'perfect'
+      Comment.where(comment: content)
+    elsif method == 'forward'
+      Comment.where('comment LIKE ?', content + '%')
+    elsif method == 'backward'
+      Comment.where('comment LIKE ?', '%' + content)
+    else
+      Comment.where('comment LIKE ?', '%' + content + '%')
+    end
+  end
 end
