7bdacfafc95231c581e13ba69ad2cdc0ff804925
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-06-05 09:42:59 +0000

    [Fix]map

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,13 @@ require_relative "boot"
 
 require "rails/all"
 
-config.require_master_key = true
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MinnanoGoshuin
   class Application < Rails::Application
+    config.require_master_key = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
