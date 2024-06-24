d93a1224bbafaeb770b3085e5db73792eb1f39cf
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-06-24 20:43:35 +0900

    fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,5 +312,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.x_forwarded_proto = 'https' if Rails.env.production?
+  # config.x_forwarded_proto = 'https' if Rails.env.production?
 end
